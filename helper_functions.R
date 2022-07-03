ff7315e0b16c8283160f5506852922d981d57952
nicolasmollier <nicolas.mollier@gmx.de>
2022-07-03 15:38:23 +0200

    add analsis of new sections

--- helper_functions.R
+++ helper_functions.R
@@ -11,25 +11,31 @@ larger_smaller_than <- function(vector, threshold, larger = TRUE){
 
 # return table with things that work well and that could be improved for a given ressort
 ressort_begründungen <- function(df, ressort_name, one_column = FALSE){
-  ressort_temp <- df %>% 
-    filter(ressort == ressort_name)%>% 
-    select(1,3, 4)
-  
-  colnames(ressort_temp) <- c("ressort", "Was klappt gut?", "Was ist ausbaufähig?")
-  assign(paste("ressort_", ressort_name), ressort_temp, parent.frame())
-  
-  if(one_column){
-    ressort_output <- ressort_temp %>% 
-      select(2:3) %>% 
-      kable() %>% 
-      column_spec(1, width_min = '7.8in') %>% 
-      kable_styling(font_size = 12)
-  } else{
-    ressort_output <- ressort_temp %>% 
-      select(2:3) %>% 
-      kable() %>% 
-      column_spec(1:2, width_min = '3.9in') %>% 
-      kable_styling(font_size = 12)
+  ressort_names <- df$ressort_group %>% 
+    unique()
+  if(ressort_name %in% ressort_names){
+    ressort_temp <- df %>% 
+      filter(ressort_group == ressort_name)%>% 
+      select(1,3, 4)
+    
+    colnames(ressort_temp) <- c("ressort", "Was klappt gut?", "Was ist ausbaufähig?")
+    assign(paste("ressort_", ressort_name), ressort_temp, parent.frame())
+    
+    if(one_column){
+      ressort_output <- ressort_temp %>% 
+        select(2:3) %>% 
+        kable() %>% 
+        column_spec(1, width_min = '7.8in') %>% 
+        kable_styling(font_size = 12)
+    } else{
+      ressort_output <- ressort_temp %>% 
+        select(2:3) %>% 
+        kable() %>% 
+        column_spec(1:2, width_min = '3.9in') %>% 
+        kable_styling(font_size = 12)
+    }
+  } else {
+    ressort_output <- NULL
   }
   
   
@@ -41,27 +47,33 @@ ressort_begründungen <- function(df, ressort_name, one_column = FALSE){
 
 # return table with things that work well and that could be improved for a given ressortleiter
 ressortleiter_begründungen <- function(df, ressort_name, one_column = TRUE){
-  ressortleiter_temp <- df %>% 
-    filter(ressort == ressort_name)%>% 
-    select(1,6)
-  
-  colnames(ressortleiter_temp) <- colnames(ressortleiter_temp) %>% 
-    substring(4)
-  
-  assign(paste("ressortleiter_", ressort_name), ressortleiter_temp, parent.frame())
-  
-  if(one_column){
-    ressortleiter_output <- ressortleiter_temp %>% 
-      select(2) %>% 
-      kable() %>% 
-      column_spec(1, width_min = '7.8in') %>% 
-      kable_styling(font_size = 12, full_width = TRUE)
+  ressort_names <- df$ressort_group %>% 
+    unique()
+  if(ressort_name %in% ressort_names){
+    ressortleiter_temp <- df %>% 
+      filter(ressort_group == ressort_name)%>% 
+      select(1,7)
+    
+    colnames(ressortleiter_temp) <- colnames(ressortleiter_temp) %>% 
+      substring(4)
+    
+    assign(paste("ressortleiter_", ressort_name), ressortleiter_temp, parent.frame())
+    
+    if(one_column){
+      ressortleiter_output <- ressortleiter_temp %>% 
+        select(2) %>% 
+        kable() %>% 
+        column_spec(1, width_min = '7.8in') %>% 
+        kable_styling(font_size = 12, full_width = TRUE)
+    } else {
+      ressortleiter_output <- ressortleiter_temp %>% 
+        select(2) %>% 
+        kable() %>% 
+        column_spec(1:2, width_min = '3.9in') %>% 
+        kable_styling(font_size = 12, full_width = TRUE)
+    }
   } else {
-    ressortleiter_output <- ressortleiter_temp %>% 
-      select(2) %>% 
-      kable() %>% 
-      column_spec(1:2, width_min = '3.9in') %>% 
-      kable_styling(font_size = 12, full_width = TRUE)
+    ressortleiter_output <- NULL
   }
   
   
