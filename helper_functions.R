45ce902efeccec06c89d6ef1dd0fd057679c3bb2
nicolasmollier <nicolas.mollier@gmx.de>
2022-11-12 18:28:20 +0100

    update

--- helper_functions.R
+++ helper_functions.R
@@ -16,24 +16,18 @@ ressort_begründungen <- function(df, ressort_name, one_column = FALSE){
   if(ressort_name %in% ressort_names){
     ressort_temp <- df %>% 
       filter(ressort_group == ressort_name)%>% 
-      select(1,3, 4)
+      select(1,3, 4) 
     
     colnames(ressort_temp) <- c("ressort", "Was klappt gut?", "Was ist ausbaufähig?")
     assign(paste("ressort_", ressort_name), ressort_temp, parent.frame())
     
-    if(one_column){
-      ressort_output <- ressort_temp %>% 
-        select(2:3) %>% 
-        kable() %>% 
-        column_spec(1, width_min = '7.8in') %>% 
-        kable_styling(font_size = 12)
-    } else{
-      ressort_output <- ressort_temp %>% 
-        select(2:3) %>% 
-        kable() %>% 
-        column_spec(1:2, width_min = '3.9in') %>% 
-        kable_styling(font_size = 12)
-    }
+    ressort_output <- ressort_temp %>% 
+      select(2:3) %>% 
+      remove_empty(which = "rows") %>% 
+      DT::datatable(
+        #colnames = rep("", ncol(.)),
+        options = list(dom = 'tp', pageLength = 10, ordering = FALSE)
+      ) 
   } else {
     ressort_output <- NULL
   }
@@ -52,26 +46,20 @@ ressortleiter_begründungen <- function(df, ressort_name, one_column = TRUE){
   if(ressort_name %in% ressort_names){
     ressortleiter_temp <- df %>% 
       filter(ressort_group == ressort_name)%>% 
-      select(1,7)
+      select(1,7) 
     
     colnames(ressortleiter_temp) <- colnames(ressortleiter_temp) %>% 
       substring(4)
     
     assign(paste("ressortleiter_", ressort_name), ressortleiter_temp, parent.frame())
     
-    if(one_column){
-      ressortleiter_output <- ressortleiter_temp %>% 
-        select(2) %>% 
-        kable() %>% 
-        column_spec(1, width_min = '7.8in') %>% 
-        kable_styling(font_size = 12, full_width = TRUE)
-    } else {
-      ressortleiter_output <- ressortleiter_temp %>% 
-        select(2) %>% 
-        kable() %>% 
-        column_spec(1:2, width_min = '3.9in') %>% 
-        kable_styling(font_size = 12, full_width = TRUE)
-    }
+    ressortleiter_output <- ressortleiter_temp %>% 
+      select(2) %>% 
+      remove_empty(which = "rows") %>% 
+      DT::datatable(
+        #colnames = rep("", ncol(.)),
+        options = list(dom = 'tp', pageLength = 10, ordering = FALSE)
+      ) 
   } else {
     ressortleiter_output <- NULL
   }
